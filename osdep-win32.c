46dab113fed54cf00146ddc1dac5ec8880051b26
Xuntao Chi <chotaotao1qaz2wsx@gmail.com>
2026-05-10 16:23:59 +0800

    build: detect native win32 platform

---
+++ osdep-win32.c
@@ -0,0 +1,34 @@
+/* $OpenBSD$ */
+
+/*
+ * Copyright (c) 2026 tmux Windows port contributors
+ *
+ * Permission to use, copy, modify, and distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ */
+
+#include <sys/types.h>
+
+#include <event.h>
+#include <stdlib.h>
+
+#include "tmux.h"
+
+char *
+osdep_get_name(__unused int fd, __unused char *tty)
+{
+	return (NULL);
+}
+
+char *
+osdep_get_cwd(__unused int fd)
+{
+	return (NULL);
+}
+
+struct event_base *
+osdep_event_init(void)
+{
+	return (event_init());
+}
